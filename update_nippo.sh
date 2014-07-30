ce9838919279fba4aa34bd4fba9ff012d236f64b
soe-j <soeda@val.co.jp>
2014-07-30 18:26:20 +0900

    update時にコミットメッセージ追加できるよう変更

--- update_nippo.sh
+++ update_nippo.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 git checkout $owner_name"_"`date +%Y%m%d`
 git add `date +%Y/%m/%d`"/"
+<<<<<<< Updated upstream
 git commit
+=======
+git commit -m $1
+>>>>>>> Stashed changes
 git push origin $owner_name"_"`date +%Y%m%d`
